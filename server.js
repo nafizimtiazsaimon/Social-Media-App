2d8fde1fe8557b267c545e0a388a39531ce6a577
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-17 21:41:23 +0600

    API routes for signup and login

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ const PORT = process.env.PORT || 3000;
 connectDb();
 
 nextApp.prepare().then(() => {
+  app.use("/api/signup", require("./api/signup"));
+  app.use("/api/auth", require("./api/auth"));
+
   app.all("*", (req, res) => handle(req, res));
+
   server.listen(PORT, (err) => {
     if (err) throw err;
-    //console.log(`Express server running on ${PORT}`);
+    //console.log(`Server running on ${PORT}`);
   });
 });
 //npm run dev

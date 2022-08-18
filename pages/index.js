a7d6ac50591a355c36e8b87ca9d72a68aa619b20
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-18 13:25:15 +0600

    getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,23 @@ import React from "react";
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
 
-function Index() {
-  return <div>Homepage</div>;
+function Index({ user, postsData, errorLoading }) {
+  return (
+    <div>
+      {posts &&
+        psots.length > 0 &&
+        posts.map((post) => <h1 key={post.id}>{post.title}</h1>)}
+    </div>
+  );
 }
+
+Index.getInitialProps = async (ctx) => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+  const { name } = ctx.query;
+  console.log(name);
+
+  return { postsData: res.data };
+};
 
 export default Index;
